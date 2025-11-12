73bf8ca52130e68b1e41ccac53afbbd389880ad3
Frro242nt <dmitriy.jakutov@gmail.com>
2025-11-12 11:58:24 +0200

    fix2

--- Lab05/main.cpp
+++ Lab05/main.cpp
@@ -3,7 +3,7 @@
 #include <limits>
 
 using namespace std;
-
+//Point 1
 // Перегруженная функция для двух чисел
 int f(int a, int b) {
     cout << "A function is used to combine two numbers." << endl;
@@ -15,15 +15,50 @@ int f(int a, int b) {
     }
     return a * mult + b;
 }
-// Перегруженная функция для одного числа - противоположное число
+// Перегруженная функция для одного числа
 int f(int a) {
     cout << "The function of the opposite number is used" << endl;
     return -a;
 }
+//Point 2
+void print() {
+    cout << "Point 2: \n";
+
+    cout << "Data types:\n";
+
+    cout << "int (sides):" << endl;
+    cout << "Size: " << sizeof(int) * 8 << " bit" << endl;
+    cout << "Min: " << numeric_limits<int>::lowest() << endl;
+    cout << "Max: " << numeric_limits<int>::max() << endl;
+
+    cout << "double (square):" << endl;
+    cout << "Size: " << sizeof(double) * 8 << " bit" << endl;
+    cout << "Min: " << scientific << numeric_limits<double>::lowest() << endl;
+    cout << "Max: " << scientific << numeric_limits<double>::max() << endl;
+    cout << fixed;
+}
+
+void calc() {
+    int a, b, c;
+
+    cout << "Enter the sides of the triangle: ";
+    cin >> a >> b >> c;
+
+    if (a <= 0 || b <= 0 || c <= 0 || a + b <= c || a + c <= b || b + c <= a) {
+        cout << "Error" << endl;
+        return;
+    }
+
+    double p = (a + b + c) / 2.0;
+    double area = sqrt(p * (p - a) * (p - b) * (p - c));
+
+    cout << "Square: " << area << " (Type: double)" << endl;
+}
+
 int main() {
     int choice;
     int a, b, c;
-
+    cout << "Point 1: \n";
     cout << "Select point (1 or 2): ";
     cin >> choice;
 
@@ -73,6 +108,7 @@ int main() {
     else {
         cout << "Wrong choice!" << endl;
     }
-
+    print();
+    calc();
     return 0;
 }
